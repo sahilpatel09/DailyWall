0a53ab2fca991eec5638a9e9b1a8455da9bae957
Sahil Patel <smppatel999@gmail.com>
2024-02-08 21:00:32 -0500

    Fix: styles and add capitalizeFirstLetter func

--- app.vue
+++ app.vue
@@ -1,47 +1,26 @@
 <template>
-  <div class="container mx-auto">
-    <div class="flex justify-content-center mainRow">
-      <div class="col-md-6">
-        <h2>Bing Wallpapers Viewer</h2>
-        <p> I have made this just to learn axios with vue 3. It uses the Bing daily image archive API. I could max fetch
-          only 8 images from the api. Feel free to use it and contribute to design. </p>
+  <div class="mx-auto bg-gray-900">
+    <section class="body-font">
+      <div class="container px-5 py-16 mx-auto">
+        <div class="flex flex-col text-center w-full">
+          <h2 class="text-xs text-indigo-400 tracking-widest font-medium title-font mb-1">Bing Wallpapers</h2>
+          <h1 class="sm:text-3xl text-xl font-medium title-font mb-4 text-white">DailyWall</h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-400">Welcome to the DailyWall Wallpapers Viewer, a
+            dynamic platform designed to bring you daily doses of visual
+            inspiration. Created with the intent of mastering Axios with Vue 3, our application taps into the Bing daily
+            image archive API, delivering a curated selection of captivating wallpapers straight to your screen.</p>
+        </div>
       </div>
-
-      <div class="col-md-6">
-        <form class="form-inline" @submit.prevent="newSearch">
-          <div class="form-group form-inline">
-            <label class="sr-only" for="pwd">Zones</label>
-            <select class="form-control" v-model="zone">
-              <option>en-US</option>
-              <option>en-CA</option>
-            </select>
-          </div>
-          <div class="form-group form-inline pt-3">
-            <label class="sr-only" for="pwd">No of Images ( Max 8 )</label>
-            <select class="form-control" v-model="numberofImages">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
-            </select>
-          </div>
-          <button type="submit" class="btn btn-danger ml-3 mt-2" @submit.prevent="newSearch">Submit</button>
-        </form>
-      </div>
-    </div>
-    <section class="text-gray-600 body-font">
-      <div class="px-5 py-24 mx-auto">
+      <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-wrap -mx-2 -mb-10 text-center">
           <div class="sm:w-1/2 mb-10 px-4" v-for="i, item in imageList" :key="item">
             <div class="rounded-lg overflow-hidden">
-              <p class="max-w-lg text-xl font-semibold leading-loose text-gray-900 dark:text-white text-left">{{ i.title
-              }}</p>
-              <p class="text-md text-gray-900 dark:text-white text-left">{{ i.copyright }}</p>
-              <img alt="content" class="object-cover object-center h-full w-full" :src="'https://bing.com' + i.url">
+              <div class="text-center py-5">
+                <p class="text-xl font-semibold leading-loose text-gray-900 dark:text-white font-sans">
+                  {{ capitalizeFirstLetter(i.title) }}</p>
+                <img alt="content" class="object-cover object-center h-full w-full" :src="'https://bing.com' + i.url">
+                <p class="text-md text-gray-900 dark:text-white p-2">{{ i.copyright }}</p>
+              </div>
             </div>
           </div>
         </div>
@@ -62,4 +41,14 @@ onMounted(async () => {
 function getImgUrl(imagePath) {
   return "https://bing.com" + imagePath;
 }
+
+function capitalizeFirstLetter(sentence) {
+  let words = sentence.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+  let capitalizedSentence = words.join(" ");
+  return capitalizedSentence;
+}
+
 </script>
